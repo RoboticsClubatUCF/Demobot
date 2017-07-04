1fa69946f557a541cc01dcd39f4e65de8106f378
Wesley Fletcher <wesfletch@gmail.com>
2017-07-04 05:02:44 -0400

    Adding file that detects the shapes of contours
    significantly more effective than what was being done to chase the ball initially, could be useful when dealing with more than one object
    adaptive thresholding makes changes in lighting easier to deal with

---
+++ OpenCV/detector.py
@@ -0,0 +1,46 @@
+from shapedetector import ShapeDetector
+import imutils
+import cv2
+
+camera = cv2.VideoCapture(0)
+camera.set(4, 240)
+camera.set(3, 320)
+
+while True:
+
+	(grabbed, image) = camera.read() #pull video from cv camera
+	resized = imutils.resize(image, width=320)
+	
+	ratio = image.shape[0] / float(resized.shape[0])
+
+	#image processing
+	gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+	blurred = cv2.GaussianBlur(gray, (5,5), 0)
+	thresh = cv2.adaptiveThreshold(blurred,175, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2) #adaptive thresholding http://docs.opencv.org/trunk/d7/d4d/tutorial_py_thresholding.html
+
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+	sd = ShapeDetector()
+
+	for c in cnts:
+
+		M = cv2.moments(c)
+		if M["m00"] != 0:
+			cX = int((M["m10"] / M["m00"]))
+			cY = int((M["m01"] / M["m00"]))
+		else:
+			cX, cY = 0,0	
+
+		shape = sd.detect(c)
+
+		c = c.astype("int")
+		cv2.drawContours(image, [c], -1, (0,255,0), 1)
+		cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, .5, (255,255,255), 2)
+
+	cv2.imshow("Image", image)
+	cv2.waitKey(1)	
+
+camera.release()
+cv2.destroyAllWindows()		
+
