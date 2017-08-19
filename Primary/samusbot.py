a6b0c06a5f05786eceb48229b0297a59f10d9f09
Joe Peaden <joe.peaden@gmail.com>
2017-08-19 13:13:28 -0500

    keyboard interrupt now shuts down motors

--- Primary/samusbot.py
+++ Primary/samusbot.py
@@ -5,6 +5,10 @@
 # Screen is divided into 3 sections, motors are directed depending
 # on which section of screen the ball is in
 
+# servo stays extended if ball is out iof view
+# create keyboard interrupt to stop effciently
+# turn for last known location
+
 # SAMUSBOT
 
 import cv2
@@ -22,8 +26,6 @@ gpio.setup(12, gpio.OUT) # pin 12 used
 p = gpio.PWM(12, 50) # setting pin to pulse-width modulation with frequency of 50 hertz 
 p.start(7.5) # sets duty cycle to 7.5 (neutral)
 
-def empty(z):
-    pass
 ##### Functions #####
 
 def turnLeft():
@@ -38,10 +40,15 @@ def forward():
     #print("Ball is in center")
     ser.write(chr(int('127'))) #motor1 max forward
     ser.write(chr(int('255'))) #motor2 max forward
-def search():
+def search(dir):
     #print("SEARCHING")
-    ser.write(chr(int('127'))) #motor2 max forward
-    ser.write(chr(int('128'))) #motor2 max backward
+    if dir == 'right':
+        ser.write(chr(int('127'))) #motor2 max forward
+        ser.write(chr(int('128'))) #motor2 max backward
+    else:
+        ser.write(chr(int('1'))) #motor2 max backward
+        ser.write(chr(int('225'))) #motor2 max forward
+        
 def closeClaw():  
     ser.write(chr(int('0'))) # stop motors
     p.ChangeDutyCycle(2.5) # set servo to 0 degrees (closed position for claw)
@@ -65,6 +72,7 @@ twothird = third + third
 minArea = 1 # minimum area to track object
 criticalArea = 13000 # area to close claws (may be too large)
 clawOpen = 0 # 0 is false, 1 is true, shows if claw is open
+lastdir = 'right' # last known direction of object that's tracked (for searching)
 
 ##### Camera/serial setup #####
 
@@ -77,7 +85,7 @@ if cam.isOpened():
 # Begin operation loop    
 openClaw()
 while (True):
-  
+    try:
 
 ##### Setting up image tracking #####
 
@@ -86,53 +94,53 @@ while (True):
     # taped ball (neon orange): min: (0,71,179), max: (11, 255, 255)
     # dark green taylormade ball: min: (57,137,54), max: (73,255,255)
 
-    ret,frame = cam.read()
-    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    image_mask=cv2.inRange(hsv,np.array([121,39,98]),np.array([240,255,255]))   
-    erode=cv2.erode(image_mask,None,iterations=3)
-    moments=cv2.moments(erode,True)
-    area=moments['m00']
+        ret,frame = cam.read()
+        hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        image_mask=cv2.inRange(hsv,np.array([121,39,98]),np.array([240,255,255]))   
+        erode=cv2.erode(image_mask,None,iterations=3)
+        moments=cv2.moments(erode,True)
+        area=moments['m00']
 
 ##### Movement decision making #####
 
         
-    if moments['m00'] >=minArea:
-        x=moments['m10']/moments['m00']
-        y=moments['m01']/moments['m00']
-        cv2.circle(frame,(int(x),int(y)),5,(0,255,0),-1)
+        if moments['m00'] >=minArea:
+            x=moments['m10']/moments['m00']
+            y=moments['m01']/moments['m00']
+            cv2.circle(frame,(int(x),int(y)),5,(0,255,0),-1)
         # if area is large enough, stop, close claw
-        if (area >= criticalArea):
-  	    if(clawOpen == 1):
-                clawOpen = closeClaw()                
-        else:	
-	    if (clawOpen == 0):
-	 	clawOpen = openClaw()
+            if (area >= criticalArea):
+                if(clawOpen == 1):
+                    clawOpen = closeClaw()                
+            else:	
+                if (clawOpen == 0):
+                    clawOpen = openClaw()
             # if ball on right third of camera
-            if (x>twothird):
-             	turnRight()
+                if (x>twothird):
+                	lastdir = 'right'
+                 	turnRight()
             # if ball on left third of camera
-            elif (x<third):
-                turnLeft()
+                elif (x<third):
+                    lastdir = 'left'
+                    turnLeft()
             # if ball is in center section of camera
-            else:
-                forward()
-
+                else:
+                    forward()
+    
     # if ball is out of camera view    
-    if (area<minArea ):
-        search()
+        if (area<minArea ):
+            search(lastdir)
 
    # VID WINDOWS DISABLED : Won't run on startup without disabled windows
    # Shows two video windows    
-   # cv2.imshow('eroded',erode)
-   # cv2.imshow('frame',frame)
-   # if cv2.waitKey(1)==27:
-   #     break
-
-
-p.stop # stopping PWM
-gpio.cleanup() # resets GPIO pin
-#cv2.destroyAllWindow() # not needed since vid windows are disabled
-cam.release()
-
-
+        #cv2.imshow('eroded',erode)
+        #cv2.imshow('frame',frame)
+        #if cv2.waitKey(1)==27:
+         #   break
+    except(KeyboardInterrupt):
+        ser.write(chr(int('0')))
+        p.stop # stopping PWM
+        gpio.cleanup() # resets GPIO pin
+        #cv2.destroyAllWindows() # not needed since vid windows are disabled
+        cam.release()
 
