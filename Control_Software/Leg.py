aa80cd387ea8882404ebdc2ba1f6536a0fb11f08
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-03-03 15:18:14 -0500

    changes to Leg

--- Control_Software/Leg.py
+++ Control_Software/Leg.py
@@ -15,13 +15,9 @@
 import RPi.GPIO as GPIO
 import time
 
-# SERVO controls:
-# 7.5 neutral, 2.5 is zero, 12.5 is 180
-
-# Leg class for grouping servos and making things easier
 class Leg():
 
-    def __init__(self, name, hs_servo_pin, vs_servo_pin, k_servo_pin, adj_leg):
+    def __init__(self, name, hs_servo_pin, vs_servo_pin, k_servo_pin):
         # hs for horizontal shoulder
         # vs for vertical shoulder
         # adj_leg is the adjacent leg
@@ -31,24 +27,24 @@ class Leg():
         horizontalShoulder = Servo(hs_servo_pin)
         verticalShoulder = Servo(vs_servo_pin)
         knee = Servo(k_servo_pin)
-        
 
-        #self.k_servo = GPIO.PWM(k_servo_pin, 50)
-        #k_servo.start(7.5)
-
-        #self.adj_leg = adj_leg
-
+    # step(self)
+    # step forward
     def step(self):
         verticalShoulder.setPosition(180)
         horizontalShoulder.setPosition(180)
         print("Stepping {} forward".format(name))
 
+    # pull(self)
+    # pull forward
     def pull(self):
         verticalShoulder.setPosition(90)
         horizontalShoulder.setPosition(90)
         print("Pulling {} backward".format(name))
 
-    def cleanUp():
+    # cleanUp(self)
+    # stop all servos in Leg
+    def cleanUp(self):
         horizontalShoulder.stop
         verticalShoulder.stop
         knee.stop
